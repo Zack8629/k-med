e2db3bd0ffb7ed69432e81b125c5f4c1efbfa5d2
Zack <Zack0094@gmail.com>
2022-07-07 00:03:56 +0300

    Done v 0.2 fix changes

--- a_scripts/main.py
+++ a_scripts/main.py
@@ -1,8 +1,10 @@
-import openpyxl
-import pandas as pd
+from openpyxl import load_workbook
+from pandas import read_excel
 
-ing_file = 'файлы/списки от СК/список ингосстрах.XLS'
-result_file = 'Файл для заполнения.xlsx'
+result_file = 'готовый файл.xlsx'
+
+ingosstrakh_file = 'список ингосстрах.XLS'
+cogaz_file = 'списки от СК/список согаз.xls'
 
 
 def determine_gender(_val):
@@ -24,86 +26,152 @@ def determine_gender(_val):
         return title_val
 
 
-def get_data(file: str, sheet_name=0):
-    data_frame = pd.read_excel(file, sheet_name=sheet_name)
+def get_list_policies(writable_sheet, last_line_file, policies_column=10):
+    start_line_file = 2
 
-    exclude_column = [8, 9, 10, 13, 14, 15, 16, 17]
+    list_policies = []
+    for line_pol in range(start_line_file, last_line_file + 1):
+        value_cell = writable_sheet.cell(row=line_pol, column=policies_column).value
+        list_policies.append(value_cell)
 
-    start_line = 12
-    last_line = data_frame.shape[0]
-
-    start_column = 1
-    last_column = data_frame.shape[1]
-
-    list_data = []
-    for num_line in range(start_line, last_line):
-        val_line = str(data_frame.iloc[num_line, start_column])
-        if val_line == 'nan' or val_line.isspace():
-            break
-
-        data_line = []
-        for num_column in range(start_column, last_column):
-            cell_value = str(data_frame.iloc[num_line, num_column])
-
-            if cell_value == 'nan' or cell_value.isspace() or num_column in exclude_column:
-                continue
-
-            valid_value = determine_gender(cell_value)
-            data_line.append(valid_value)
-
-        list_data.append(data_line)
-
-    return list_data
+    return list_policies
 
 
-def write_data_to_file(file_to_write: str, data, sheet_to_write=0):
-    dict_to_write = {
-        0: 10,
-        1: 2,
-        2: 3,
-        3: 4,
-        4: 6,
-        5: 5,
-        6: 22,
-        7: 7,
-        8: 8,
-        9: 23,
-    }
-
-    writable_file = openpyxl.load_workbook(file_to_write, read_only=False, keep_vba=True)
-    writable_sheet = writable_file.worksheets[sheet_to_write]
-
-    last_line_the_file = writable_sheet.max_row
-    line_to_write = last_line_the_file + 1
-
-    first_column = writable_sheet.min_column
-
-    last_serial_number = last_line_the_file - 1
-    if last_serial_number == 0:
-        last_serial_number = 1
-
-    for idx_line, line in enumerate(data):
-        writable_sheet.cell(row=line_to_write + idx_line,
-                            column=first_column).value = last_serial_number + idx_line
-
-        for idx_value, value in enumerate(line):
-            writable_sheet.cell(row=line_to_write + idx_line,
-                                column=dict_to_write[idx_value]).value = value
-
+def save_file_to_exel(file_to_read, writable_file, file_to_write, sheet_num=0):
     writable_file.save(file_to_write)
 
-    sheet_name = writable_file.sheetnames[0]
+    sheet_name = writable_file.sheetnames[sheet_num]
 
-    data_frame = pd.read_excel(file_to_write, sheet_name=0, engine='openpyxl')
+    data_frame = read_excel(file_to_write, sheet_name=sheet_num)
     data_frame.to_excel(file_to_write, sheet_name=sheet_name, encoding='utf-8', index=False)
 
-    name_file = file_to_write.split(sep='/')
-    print(f'Write DATA to {name_file[-1]} DONE!')
+    print(f'Data from "{file_to_read}" is written to "{file_to_write}"!')
 
+
+def pars_ingosstrakh(file_to_read: str, file_to_write: str, sheet_num=0):
+    def get_data_to_write():
+        data_frame = read_excel(file_to_read, sheet_name=sheet_num)
+
+        exclude_column = [8, 9, 10, 13, 14, 15, 16, 17]
+
+        start_line = 12
+        last_line = data_frame.shape[0]
+
+        start_column = 1
+        last_column = data_frame.shape[1]
+
+        list_data = []
+        for num_line in range(start_line, last_line):
+            val_line = str(data_frame.iloc[num_line, start_column])
+            if val_line == 'nan' or val_line.isspace():
+                break
+
+            data_line = []
+            for num_column in range(start_column, last_column):
+                cell_value = str(data_frame.iloc[num_line, num_column])
+
+                if cell_value == 'nan' or cell_value.isspace() or num_column in exclude_column:
+                    continue
+
+                valid_value = determine_gender(cell_value)
+                data_line.append(valid_value)
+
+            list_data.append(data_line)
+
+        return list_data
+
+    def write_data():
+        dict_to_write = {
+            0: 10,
+            1: 2,
+            2: 3,
+            3: 4,
+            4: 6,
+            5: 5,
+            6: 22,
+            7: 7,
+            8: 8,
+            9: 23,
+        }
+
+        writable_file = load_workbook(file_to_write, read_only=False, keep_vba=True)
+        writable_sheet = writable_file.worksheets[sheet_num]
+
+        last_line_the_file = writable_sheet.max_row
+        line_to_write = last_line_the_file + 1
+
+        first_column = writable_sheet.min_column
+
+        policies = get_list_policies(writable_sheet=writable_sheet,
+                                     last_line_file=last_line_the_file,
+                                     policies_column=10)
+        print(policies)
+
+        data_to_write = get_data_to_write()
+
+        next_line = 0
+        for line in data_to_write:
+            if line[0] in policies:
+                print(f'The policy: "{line[0]}" is already in the recorded file')
+                continue
+
+            writable_sheet.cell(row=line_to_write + next_line,
+                                column=first_column).value = last_line_the_file + next_line
+
+            for idx_value, value in enumerate(line):
+                writable_sheet.cell(row=line_to_write + next_line,
+                                    column=dict_to_write[idx_value]).value = value
+
+            next_line += 1
+
+        save_file_to_exel(file_to_read, writable_file, file_to_write)
+
+    write_data()
+
+
+def pars_cogaz(file_to_read: str, file_to_write: str, sheet_num=0):
+    def get_data_to_write():
+        data_frame = read_excel(file_to_read, sheet_name=sheet_num)
+
+        exclude_column = []
+
+        start_line = 12
+        last_line = data_frame.shape[0]
+
+        start_column = 1
+        last_column = data_frame.shape[1]
+
+        list_data = []
+        for num_line in range(start_line, last_line):
+            val_line = str(data_frame.iloc[num_line, start_column])
+            if val_line == 'nan' or val_line.isspace():
+                break
+
+            data_line = []
+            for num_column in range(start_column, last_column):
+                cell_value = str(data_frame.iloc[num_line, num_column])
+
+                if cell_value == 'nan' or cell_value.isspace() or num_column in exclude_column:
+                    continue
+
+                valid_value = determine_gender(cell_value)
+                data_line.append(valid_value)
+
+            list_data.append(data_line)
+
+        return list_data
+
+    print(get_data_to_write())
 
 def main():
-    data = get_data(ing_file)
-    write_data_to_file(result_file, data)
+    num_runs = 2
+    print(f'num_runs = {num_runs}')
+
+    print()
+    for i in range(num_runs):
+        # pars_ingosstrakh(ingosstrakh_file, result_file, sheet_num=0)
+        print()
+        pars_cogaz(cogaz_file, result_file, sheet_num=0)
 
 
 if __name__ == '__main__':
